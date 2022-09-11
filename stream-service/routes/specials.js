aabe33d60fd6e93c846a08250f7f3c482b40aa78
Abhay Pratap Yadav <98642540+Plycedes@users.noreply.github.com>
2022-09-11 15:21:58 +0530

    Images
    Loading images using microservices

--- stream-service/routes/specials.js
+++ stream-service/routes/specials.js
@@ -5,16 +5,16 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   let jsonResponse = {
     "handsetCards": [
-      { title: 'Card 1', cols: 1, rows: 1 },
-      { title: 'Card 2', cols: 1, rows: 1 },
-      { title: 'Card 3', cols: 1, rows: 1 },
-      { title: 'Card 4', cols: 1, rows: 1 }
+      { imageName: 'img1', title: 'Massive discount on guns based on users accuracy', cols: 1, rows: 1 },
+      { imageName: 'img2', title: 'Mark 3 now available on market', cols: 1, rows: 1 },
+      { imageName: 'img3', title: 'Buy 2 get 1 free', cols: 1, rows: 1 },
+      { imageName: 'img4', title: 'Only 1 left in stock', cols: 1, rows: 1 }
     ],
     "webCards": [
-      { title: 'Card 1', cols: 2, rows: 1 },
-      { title: 'Card 2', cols: 1, rows: 1 },
-      { title: 'Card 3', cols: 1, rows: 2 },
-      { title: 'Card 4', cols: 1, rows: 1 }
+      { imageName: 'img1', title: 'Massive discount on guns based on accuracy', cols: 2, rows: 1 },
+      { imageName: 'img2', title: 'Mark 3 now available on market', cols: 1, rows: 1 },
+      { imageName: 'img3', title: 'Buy 2 get 1 free', cols: 1, rows: 2 },
+      { imageName: 'img4', title: 'Only 1 left in stock', cols: 1, rows: 1 }
     ]
   };
   res.json(jsonResponse);
